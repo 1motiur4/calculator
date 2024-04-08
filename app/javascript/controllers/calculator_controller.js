ac83fbc1d89dee1ae73275e636495544a56eef00
Motiur Muhith <1motiur4@gmail.com>
2024-04-08 15:58:48 -0400

    test

---
+++ app/javascript/controllers/calculator_controller.js
@@ -0,0 +1,11 @@
+import { Controller } from "@hotwired/stimulus"
+
+export default class extends Controller {
+  connect() {
+  }
+
+  print_1() {
+    const screenElement = this.element.querySelector("#calculator-screen")
+    screenElement.innerHTML += "1"
+  }
+}
